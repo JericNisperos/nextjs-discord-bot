aa5de78b7440575873faffabd6795dee6f39e45e
Jeric <jericrnisperos@gmail.com>
2024-08-20 22:34:32 +0800

    added embed

--- commands/embed.ts
+++ commands/embed.ts
@@ -1,27 +1,28 @@
-// Add this to your help.ts file or create a new file for your commands if preferred
-
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { fetchBotCommands } from "@/utils/discord-api";
 import type { executeCommand } from "@/types";
 
-export const hello = new SlashCommandBuilder()
-  .setName("hello")
-  .setDescription("Says Hello World");
+export const register = new SlashCommandBuilder()
+  .setName("embed")
+  .setDescription("Returns a list of registered commands");
 
-export const executeHello: executeCommand = async (interaction) => {
-  // Create an embed message
-  const embed = {
-    title: "Hello World",
-    description: "This is a custom embed message.",
-    color: 0x34d9d9,
-  };
+export const execute: executeCommand = async (interaction) => {
+  // Fetch the registered commands from discord api
+  const commandsList = await fetchBotCommands();
+
+  // Format
+  const fields = commandsList.data.map((c) => {
+    return { name: "/" + c.name, value: c.description + "\n \u200b" };
+  });
 
   return {
     type: 4,
     data: {
       embeds: [
         {
-          embeds: [embed],
+          color: 0x34d9d9,
+          title: "Here are the list of registered commands \n \u200b",
+          fields: fields,
         },
       ],
     },
